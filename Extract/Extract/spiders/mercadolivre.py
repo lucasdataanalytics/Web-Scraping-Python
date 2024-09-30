ac423ca109e5ca663692119a33ae70acb3e1c8f3
lucasdataanalytics <trafegoalmeida@gmail.com>
2024-09-30 16:17:18 -0300

    Update

--- Extract/Extract/spiders/mercadolivre.py
+++ Extract/Extract/spiders/mercadolivre.py
@@ -4,7 +4,12 @@ import scrapy
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
-    start_urls = ["https://lista.mercadolivre.com.br/livros-programa%C3%A7%C3%A3o"]
+    start_urls = ["https://lista.mercadolivre.com.br/data-science"]
 
     def parse(self, response):
-        pass
+        products = response.css('div.ui-search-result__content')
+        
+        for product in products:
+            yield{
+                'name': products.css('h2.ui-search-item__title.ui-search-item__group__element a::text').get()
+                }
