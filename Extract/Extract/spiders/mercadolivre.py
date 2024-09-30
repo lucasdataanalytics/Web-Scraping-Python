162bc44d462c86b2485ed4c8161adc2edbd04215
lucasdataanalytics <trafegoalmeida@gmail.com>
2024-09-30 14:05:06 -0300

    Uptade

--- Extract/Extract/spiders/mercadolivre.py
+++ Extract/Extract/spiders/mercadolivre.py
@@ -4,7 +4,7 @@ import scrapy
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
-    start_urls = ["https://lista.mercadolivre.com.br/livros-data"]
+    start_urls = ["https://lista.mercadolivre.com.br/livros-programa%C3%A7%C3%A3o"]
 
     def parse(self, response):
         pass
